da28e202567ccf9f29df34523e051840269260ff
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "mysql_flexible_databases" {
   description = "All mysql_flexible_database resources"
   value       = azurerm_mysql_flexible_database.mysql_flexible_databases
 }
+output "mysql_flexible_databases_charset" {
+  description = "List of charset values across all mysql_flexible_databases"
+  value       = [for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : v.charset]
+}
+output "mysql_flexible_databases_collation" {
+  description = "List of collation values across all mysql_flexible_databases"
+  value       = [for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : v.collation]
+}
+output "mysql_flexible_databases_name" {
+  description = "List of name values across all mysql_flexible_databases"
+  value       = [for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : v.name]
+}
+output "mysql_flexible_databases_resource_group_name" {
+  description = "List of resource_group_name values across all mysql_flexible_databases"
+  value       = [for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : v.resource_group_name]
+}
+output "mysql_flexible_databases_server_name" {
+  description = "List of server_name values across all mysql_flexible_databases"
+  value       = [for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : v.server_name]
+}
 
