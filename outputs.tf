0c823962da64cfb5521baa81e93168bed13cc9b0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:07 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "mysql_flexible_databases" {
-  description = "All mysql_flexible_database resources"
-  value       = azurerm_mysql_flexible_database.mysql_flexible_databases
-}
 output "mysql_flexible_databases_charset" {
-  description = "List of charset values across all mysql_flexible_databases"
-  value       = [for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : v.charset]
+  description = "Map of charset values across all mysql_flexible_databases, keyed the same as var.mysql_flexible_databases"
+  value       = { for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : k => v.charset }
 }
 output "mysql_flexible_databases_collation" {
-  description = "List of collation values across all mysql_flexible_databases"
-  value       = [for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : v.collation]
+  description = "Map of collation values across all mysql_flexible_databases, keyed the same as var.mysql_flexible_databases"
+  value       = { for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : k => v.collation }
 }
 output "mysql_flexible_databases_name" {
-  description = "List of name values across all mysql_flexible_databases"
-  value       = [for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : v.name]
+  description = "Map of name values across all mysql_flexible_databases, keyed the same as var.mysql_flexible_databases"
+  value       = { for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : k => v.name }
 }
 output "mysql_flexible_databases_resource_group_name" {
-  description = "List of resource_group_name values across all mysql_flexible_databases"
-  value       = [for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : v.resource_group_name]
+  description = "Map of resource_group_name values across all mysql_flexible_databases, keyed the same as var.mysql_flexible_databases"
+  value       = { for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : k => v.resource_group_name }
 }
 output "mysql_flexible_databases_server_name" {
-  description = "List of server_name values across all mysql_flexible_databases"
-  value       = [for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : v.server_name]
+  description = "Map of server_name values across all mysql_flexible_databases, keyed the same as var.mysql_flexible_databases"
+  value       = { for k, v in azurerm_mysql_flexible_database.mysql_flexible_databases : k => v.server_name }
 }
 
